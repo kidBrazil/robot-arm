006054e53ebc562278dd8b2569d38688a349d8f4
Lucas Moreira <lucas@mdev.digital>
2019-06-15 16:22:49 -0400

    Update robot-arm.ino
    Updated pins

--- robot-arm/robot-arm.ino
+++ robot-arm/robot-arm.ino
@@ -6,10 +6,10 @@ Servo ServoArmElbow; //Pin - 11
 Servo ServoArmGrasp; //Pin - 10
 
 // Servo Pins
-int const RotatePin = 13;
-int const BasePin = 12;
-int const ElbowPin = 11;
-int const GraspPin = 10;
+int const RotatePin = 2;
+int const BasePin = 3;
+int const ElbowPin = 4;
+int const GraspPin = 5;
 
 // Potentiometer Pins
 int const Joy1X = A0;
@@ -50,7 +50,7 @@ void setup() {
   // Open Serial
   Serial.begin(9600);
   // Place servo at mid point
-  ServoRotate.write(94);
+  ServoRotate.write(90);
   ServoArmBase.write(90);
   ServoArmElbow.write(90);
   ServoArmGrasp.write(90);
@@ -62,42 +62,43 @@ void setup() {
 
 // Loop
 void loop() {
-//  // put your main code here, to run repeatedly:
-// Read Potentiometer values
-Joy1XValue = analogRead(Joy1X);
-Joy1YValue = analogRead(Joy1Y);
-Joy2XValue = analogRead(Joy2X);
-Joy2YValue = analogRead(Joy2Y);
+  //  // put your main code here, to run repeatedly:
+  // Read Potentiometer values
+  Joy1XValue = analogRead(Joy1X);
+  Joy1YValue = analogRead(Joy1Y);
+  Joy2XValue = analogRead(Joy2X);
+  Joy2YValue = analogRead(Joy2Y);
 
-// Joystick 1 X  Base Rotation ----------------------------/
-Joy1XAngle = map(Joy1XValue, 0, 1023, 0, 190); // Map straight to Servo output
-ServoRotate.write(Joy1XAngle);
+  // Joystick 1 X  Base Rotation ----------------------------/
+  ServoRotate.attach(RotatePin);
+  Joy1XAngle = map(Joy1XValue, 0, 1023, 0, 190); // Map straight to Servo output
+  ServoRotate.write(Joy1XAngle);
 
-// Joystick 1 Y  Arm Raise / Lower ----------------------------/
-Joy1YAngle = map(Joy1YValue, 0, 1023, -5, 5);
-Joy1YSentAngle = CheckLimits(Joy1YLastAngle, Joy1YAngle);
-// Write to servo
-ServoArmBase.write(Joy1YSentAngle);
-// Save Servo Position
-Joy1YLastAngle = Joy1YSentAngle;
+  // Joystick 1 Y  Arm Raise / Lower ----------------------------/
+  Joy1YAngle = map(Joy1YValue, 0, 1005, -5, 5);
+  Joy1YSentAngle = CheckLimits(Joy1YLastAngle, Joy1YAngle);
+  // Write to servo
+  ServoArmBase.write(Joy1YSentAngle);
+  // Save Servo Position
+  Joy1YLastAngle = Joy1YSentAngle;
 
-// Joystick 2 X Elbow Raise / Lower ----------------------------/
-Joy2XAngle = map(Joy2XValue, 0, 1000, -10, 10);
-Joy2XSentAngle = CheckLimits(Joy2XLastAngle, Joy2XAngle);
-// Write to servo
-ServoArmElbow.write(Joy2XSentAngle);
-// Save Servo Position
-Joy2XLastAngle = Joy2XSentAngle;
+  // Joystick 2 X Elbow Raise / Lower ----------------------------/
+  Joy2XAngle = map(Joy2XValue, 0, 1000, -10, 10);
+  Joy2XSentAngle = CheckLimits(Joy2XLastAngle, Joy2XAngle);
+  // Write to servo
+  ServoArmElbow.write(Joy2XSentAngle);
+  // Save Servo Position
+  Joy2XLastAngle = Joy2XSentAngle;
 
-// Joystick 1 Y  Arm Raise / Lower ----------------------------/
-Joy2YAngle = map(Joy2YValue, 0, 1023, -10, 10);
-Joy2YSentAngle = CheckLimits(Joy2YLastAngle, Joy2YAngle);
-// Write to servo
-ServoArmGrasp.write(Joy2YSentAngle);
-// Save Servo Position
-Joy2YLastAngle = Joy2YSentAngle;
-
-delay(30);
+  // Joystick 1 Y  Arm Raise / Lower ----------------------------/
+  Joy2YAngle = map(Joy2YValue, 0, 1000, -10, 10);
+  Joy2YSentAngle = CheckLimits(Joy2YLastAngle, Joy2YAngle);
+  // Write to servo
+  ServoArmGrasp.write(Joy2YSentAngle);
+  // Save Servo Position
+  Joy2YLastAngle = Joy2YSentAngle;
+  // Delay
+  delay(30);
 }
 
 // Check to ensure limits are not passed on Servo
